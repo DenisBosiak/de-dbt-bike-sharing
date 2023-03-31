d368c2e3497f2fb6ae0bcb3e772a8f5dc42ea087
b.denis92@gmail.com <b.denis92@gmail.com>
2023-03-31 12:30:18 +0000

    Update schema.yml

--- models/staging/stg_weather.sql
+++ models/staging/stg_weather.sql
@@ -11,11 +11,11 @@ SELECT
     CAST(prectot AS float) AS precipitation				
     CAST(qv2m AS float) AS specific_humidity			
     CAST(rh2m AS float) AS relative_humidity		
-    CAST(ps	AS float) AS surface_pressure 		
+    CAST(ps AS float) AS surface_pressure 		
     CAST(t2m_range AS float) AS temperature_range			
-    CAST(ts	AS float) AS earth_skin_temperature		
-    CAST(t2mdew	AS float) AS dew_frost_point				
-    CAST(t2mwet	AS float) AS wet_bulb_temperature			
+    CAST(ts AS float) AS earth_skin_temperature		
+    CAST(t2mdew AS float) AS dew_frost_point				
+    CAST(t2mwet AS float) AS wet_bulb_temperature			
     CAST(t2m_max AS float) AS max_temperature			
     CAST(t2m_min AS float) AS min_temperature
     CAST(t2m AS float) AS avg_temperature_per_day 			    
